fa0a320271cad5f56668db3323b3a3e3ee53502f
sliitguy <it23259416@my.sliit.lk>
2025-10-02 15:20:22 +0530

    updated more endpoints

--- src/app.js
+++ src/app.js
@@ -6,7 +6,12 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.json({message: "Consolidation Service - Sparrow"})
+    res.json({message: "Sparrow: API Gateway"})
 })
+
+app.get('/health', (req, res) => {
+    res.json({message: "API Gateway is running.."})
+})
+
 
 module.exports = app
